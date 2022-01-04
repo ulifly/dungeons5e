26654b153887a8fde7199dc89bddfe12734ed521
Ulises Desentis <ulifly@hotmail.com>
2022-01-04 09:27:51 -0600

    first commit

---
+++ lib/dungeons5e.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dungeons5e/version"
+
+module Dungeons5e
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dungeons5e/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dungeons5e
+  VERSION = "0.1.0"
+end
